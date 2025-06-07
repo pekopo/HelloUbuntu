fa88e022ec60756aebaa1f7cddd3ac6b6ecd52ae
pekopo <152680794+pekopo@users.noreply.github.com>
2025-06-07 15:19:02 +0900

    first commit

---
+++ src/main/java/com/example/HelloUbuntuApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUbuntuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUbuntuApplication.class, args);
+	}
+
+}
